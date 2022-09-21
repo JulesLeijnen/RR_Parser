c4d261d6b604706620bd3d3592251708ffaed082
JulesLeijnen <60113301+JulesLeijnen@users.noreply.github.com>
2022-09-21 14:05:20 +0200

    Finished txt file chopper, added logging, mode select, exitcondition and comments

--- Main.py
+++ Main.py
@@ -1,22 +1,50 @@
-
+import re
+import logging
 
 #-------------------------------Imports-------------------------------
 #-------------------------------GlobalVars----------------------------
-
-IC = []
-Resistor = []
-Capasitor = []
-Z = []
-
+DEBUGMODE = True
 #-------------------------------GlobalVars----------------------------
+#-------------------------------Logging-------------------------------
+
+def loggingSetup(): #Setup needed logging settings
+    if DEBUGMODE:
+        logging.basicConfig(level=logging.DEBUG, filename="log.log", filemode="a", format='%(asctime)s :: %(name)s :: %(levelname)-8s :: %(message)s')
+        logging.critical("\n\n\nNew session, DEBUG=ON")
+    elif not DEBUGMODE:
+        logging.basicConfig(level=logging.WARNING, filename="log.log", filemode="a", format='%(asctime)s :: %(name)s :: %(levelname)-8s :: %(message)s')
+        logging.critical("\n\n\nNew session, DEBUG=OFF".format)
+    else:
+        logging.basicConfig(level=logging.DEBUG, filename="log.log", filemode="a", format='%(asctime)s :: %(name)s :: %(levelname)-8s :: %(message)s')
+        logging.critical("\n\n\nNew session, DEBUG=ERROR")
+        logging.warning("Logging not correctly initiated via DEBUGMODE variable... Somehow?")
+        logging.warning("Restoring default level=DEBUG config")
+    return
+
+#-------------------------------Logging-------------------------------
 #-------------------------------Main----------------------------------
 
 def main():
+    mode = selectMode()                                                                         #Selects what mode to use (most likely will be what manifacturer)
 
     return
 
 #-------------------------------Main----------------------------------
 #-------------------------------SeconderyFunctions--------------------
+
+def selectMode():
+    inputModeCorrect = False
+    while not inputModeCorrect:
+        inputMode = input("Select the mode you need (DE/AA/AB/AC/AD)").upper()
+        print("You have selected: '{}'".format(inputMode))
+        if inputMode in ["DE"]:
+            inputModeCorrect = True
+        else:
+            print("Invalid input, try again")
+
+    
+    
+    return inputMode
 
 def fromMiltoMM(Mil):
     return Mil/39.3700787
@@ -24,8 +52,34 @@ def fromMiltoMM(Mil):
 def fromMMtoMil(MM):
     return MM*39.3700787
 
+def txtToArray(textfilePath):
+    ReturnArray = []
+    f = open(textfilePath, "r")
+
+    FRead = f.read().replace("\n", "|").replace("\r", "|")                                      #Replace every newline with a "|" to be split there
+    array = FRead.split("|")                                                                    #Splits with every new line
+
+    for i in array:                                                                             #used to strip the trailing whitespaces and sorts them to item level
+        i.strip()
+        ReturnArray.append(re.split(r'\s{2,}', i))
+    
+    for i in ReturnArray:                                                                       #remove unneeded additions to array
+        while "" in i:
+            i.remove("")
+        while [] in i:
+            i.remove([])
+
+    while [] in ReturnArray:                                                                    #remove unneeded additions to array
+        ReturnArray.remove([])
+
+    return ReturnArray
+
 #-------------------------------SeconderyFunctions--------------------
 #-------------------------------__Main__------------------------------
 if __name__ == "__main__":
+    loggingSetup()                                                                              #Set up all logging stuff away from the main code for readability
     main()
-    exit(0)
+    exit(0)                                                                                     #When program is done, leave with exitcode 0 (no errors)
+else:
+    #TODO, log error (can't be run as a module)
+    exit(1)
--- tempTesting.py
+++ tempTesting.py
@@ -1,8 +1,27 @@
+import re
+
 f = open("Source/PickPlaceforBasismodule.txt", "r")
 FRead = f.read().replace("\n", "|").replace("\r", "|")
-FRead = " ".join(FRead.split())
-#print(" ".join(f.read().split()))
+#FRead = " ".join(FRead.split())
+# #print(" ".join(f.read().split()))
 array = FRead.split("|")
-for x in range(0, array.count('')):
-    array.remove('')
 print(array)
+emptyarray = []
+for i in array:
+    i.strip()
+    print('\n\n\n')
+    x = re.split(r'\s{2,}', i)
+    emptyarray.append(x)
+    #print(x)
+print(emptyarray)
+
+for i in emptyarray:
+    while "" in i:
+        i.remove("")
+        print("Found in: {}".format(i))
+    while [] in i:
+        i.remove([])
+print(emptyarray)
+while [] in emptyarray:
+    emptyarray.remove([])
+print(emptyarray)
